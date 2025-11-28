b630fb5342b9833e98e119294f596ba608b2ed4b
Ronald Flores <ronsf30@gmail.com>
2025-11-28 19:13:02 -0400

    Use python3 instead of python for Linux compatibility

--- Workload/backend/run.ps1
+++ Workload/backend/run.ps1
@@ -1,6 +1,6 @@
 # Install requirements
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Run the server
 # reload=True allows auto-reload on code changes
-python -m uvicorn main:app --host 0.0.0.0 --port 8080 --reload
+python3 -m uvicorn main:app --host 0.0.0.0 --port 8080 --reload
